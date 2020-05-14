9569344c89d993ea5476cc84ed7d3483aec8b925
Vusal Ismayilov <vusali@crbn.az>
2020-05-14 20:54:16 +0400

    Add main page

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
+
+# Routes
 @app.route('/')
 def index():
-    return render_template('index.html')
+    globalSummary = getGlobalStatistics()
+    countryList = getAllCountries()
+    countryList.sort(key=lambda x: x['Country'])
+    return render_template('index.html', datas=globalSummary, countries=countryList)
+
+
+# Helper Functions
+def getGlobalStatistics():
+    return requests.get('https://api.covid19api.com/summary').json()['Global']
+
+
+def getAllCountries():
+    return requests.get('https://api.covid19api.com/countries').json()
+
 
 if __name__ == '__main__':
-    app.run(debug=True )
+    app.run(debug=True)
