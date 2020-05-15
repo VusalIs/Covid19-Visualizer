5659495d68f68792dd269245bae86699fffe1338
Vusal Ismayilov <vusali@crbn.az>
2020-05-16 02:02:04 +0400

    Add error handling

--- app.py
+++ app.py
@@ -8,27 +8,33 @@ app = Flask(__name__)
 # Routes
 @app.route('/')
 def index():
-    globalSummary = getGlobalStatistics()
-    countryList = getAllCountries()
-    countryList.sort(key=lambda x: x['Country'])
-    return render_template('index.html', datas=globalSummary, countries=countryList)
+    try:
+        globalSummary = getGlobalStatistics()
+        countryList = getAllCountries()
+        countryList.sort(key=lambda x: x['Country'])
+        return render_template('index.html', datas=globalSummary, countries=countryList)
+    except:
+        return "Please refresh the page because we use free apis and sometimes reach at limit"
 
 
 @app.route('/country-statistic')
 def countryStatistics():
-    country = request.args.get('country')
-    dailystat = getDailyStatByCountry(country)
-    summaryOfCountry = dailystat[-1]
-    summaryOfPrevDay = dailystat[-2]
-    newCases = {
-        'NewConfirmed': summaryOfCountry['Confirmed'] - summaryOfPrevDay['Confirmed'],
-        'NewDeaths': summaryOfCountry['Deaths'] - summaryOfPrevDay['Deaths'],
-        'NewRecovered': summaryOfCountry['Recovered'] - summaryOfCountry['Recovered']
-    }
-    infectedPeopleDaily = list(map(
-        lambda value: {'y': value['Confirmed'], 'x': datetime.strptime(value['Date'],
-                                                                       "%Y-%m-%dT%H:%M:%SZ")}, dailystat))
-    return render_template('country.html', summaryData=summaryOfCountry, country=country, infectedPeopleDaily=infectedPeopleDaily, newCases=newCases)
+    try:
+        country = request.args.get('country')
+        dailystat = getDailyStatByCountry(country)
+        summaryOfCountry = dailystat[-1]
+        summaryOfPrevDay = dailystat[-2]
+        newCases = {
+            'NewConfirmed': summaryOfCountry['Confirmed'] - summaryOfPrevDay['Confirmed'],
+            'NewDeaths': summaryOfCountry['Deaths'] - summaryOfPrevDay['Deaths'],
+            'NewRecovered': summaryOfCountry['Recovered'] - summaryOfCountry['Recovered']
+        }
+        infectedPeopleDaily = list(map(
+            lambda value: {'y': value['Confirmed'], 'x': datetime.strptime(value['Date'],
+                                                                           "%Y-%m-%dT%H:%M:%SZ")}, dailystat))
+        return render_template('country.html', summaryData=summaryOfCountry, country=country, infectedPeopleDaily=infectedPeopleDaily, newCases=newCases)
+    except:
+        return "Please refresh the page because we use free apis and sometimes reach at limit"
 
 # Helper Functions
 
