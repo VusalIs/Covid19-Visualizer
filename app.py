5ed55b63435236d92f28d99fb3a20b5c9a3ceb39
Vusal Ismayilov <vusali@crbn.az>
2020-05-15 15:00:45 +0400

    Add country statistics

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from datetime import datetime
 import requests
 
 app = Flask(__name__)
@@ -13,13 +14,29 @@ def index():
     return render_template('index.html', datas=globalSummary, countries=countryList)
 
 
+@app.route('/country-statistic')
+def countryStatistics():
+    country = request.args.get('country')
+    dailystat = getDailyStatByCountry(country)
+    summaryOfCountry = dailystat[-1]
+    infectedPeopleDaily = list(map(
+        lambda value: {'y': value['Confirmed'], 'x': datetime.strptime(value['Date'],
+                                                                       "%Y-%m-%dT%H:%M:%SZ")}, dailystat))
+    return render_template('country.html', summaryData=summaryOfCountry, country=country, infectedPeopleDaily=infectedPeopleDaily)
+
 # Helper Functions
+
+
 def getGlobalStatistics():
     return requests.get('https://api.covid19api.com/summary').json()['Global']
 
 
 def getAllCountries():
     return requests.get('https://api.covid19api.com/countries').json()
+
+
+def getDailyStatByCountry(country):
+    return requests.get('https://api.covid19api.com/dayone/country/{}'.format(country)).json()
 
 
 if __name__ == '__main__':
